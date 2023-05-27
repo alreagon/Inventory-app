a5a55b65c0ebff79cc9144cf035071afa2752083
alreagon <alpharajasa@gmail.com>
2023-05-27 15:20:00 +0700

    UserRepository

--- app/src/main/java/com/zexceed/skripsiehapp/model/User.kt
+++ app/src/main/java/com/zexceed/skripsiehapp/model/User.kt
@@ -6,9 +6,8 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class User(
     val id: String = "",
-    val name: String = "",
-    val email: String = "",
     val namaUkm: String = "",
-    val listPeminjmaanId: ArrayList<String> = arrayListOf(),
+    val email: String = "",
+    val listPeminjamanId: ArrayList<String> = arrayListOf(),
     val listInventoryId: ArrayList<String> = arrayListOf()
 ) : Parcelable
--- app/src/main/java/com/zexceed/skripsiehapp/repository/UserRepository.kt
+++ app/src/main/java/com/zexceed/skripsiehapp/repository/UserRepository.kt
@@ -4,6 +4,7 @@ import android.content.ContentValues
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.ktx.userProfileChangeRequest
@@ -42,8 +43,8 @@ class UserRepository {
                     message.postValue("SUCCESS")
                     getSavedPeminjamanId()
                     getSavedPeminjaman()
-//                    getSavedInventoryId()
-//                    getSavedInventory()
+                    getSavedInventoryId()
+                    getSavedInventory()
                 } else {
                     Log.w(ContentValues.TAG, "signInWithEmail:failure", task.exception)
                     message.postValue(task.exception.toString())
@@ -102,6 +103,7 @@ class UserRepository {
     }
 
 
+    //    === Peminjaman ===
     fun getSavedPeminjamanId(): MutableLiveData<ArrayList<String>> {
         val currentUser = auth.currentUser
         currentUser?.let {
@@ -117,7 +119,7 @@ class UserRepository {
                         savedPeminjamanId.postValue(allPeminjamanId)
                         val user = snapshot.toObject(User::class.java)
                         Log.d(ContentValues.TAG, "DocumentSnapshot data: ${snapshot.data}")
-                        for (i in user!!.listPeminjmaanId) {
+                        for (i in user!!.listPeminjamanId) {
                             allPeminjamanId.add(i)
                         }
                         Log.d("TEZZ", "Current data: $allPeminjamanId")
@@ -143,17 +145,17 @@ class UserRepository {
                     }
 
                     if (snapshot != null && snapshot.exists()) {
-                        val mahasiswaKos = snapshot.toObject(User::class.java)
+                        val user = snapshot.toObject(User::class.java)
                         val allPeminjaman = arrayListOf<Peminjaman>()
                         val allPeminjamanId = arrayListOf<String>()
                         savedPeminjaman.postValue(allPeminjaman)
                         Log.d(ContentValues.TAG, "DocumentSnapshot data: ${snapshot.data}")
-                        for (i in mahasiswaKos!!.listPeminjmaanId) {
+                        for (i in user!!.listPeminjamanId) {
                             allPeminjamanId.add(i)
                         }
                         Log.d("TEZaZ", "Current data: $allPeminjamanId")
                         database.collection("peminjaman")
-                            .get().addOnSuccessListener { mSnapshot->
+                            .get().addOnSuccessListener { mSnapshot ->
                                 if (mSnapshot != null && !mSnapshot.isEmpty) {
                                     for (doc in mSnapshot) {
                                         val mPeminjaman = doc.toObject(Peminjaman::class.java)
@@ -186,11 +188,11 @@ class UserRepository {
                     if (document != null) {
                         val user = document.toObject(User::class.java)
                         Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
-                        user?.listPeminjmaanId?.add(peminjamanId)
+                        user?.listPeminjamanId?.add(peminjamanId)
                         database.collection("userUkm").document(currentUser!!.uid)
                             .set(user!!).addOnSuccessListener {
                                 Log.d("TEZ", "Current data: $it")
-                                savedPeminjamanId.postValue(user.listPeminjmaanId)
+                                savedPeminjamanId.postValue(user.listPeminjamanId)
                                 statusLiveData.postValue("SUCCESS")
                             }.addOnFailureListener {
                                 Log.d("TEZ", "Current data: $it")
@@ -219,11 +221,11 @@ class UserRepository {
                     if (document != null) {
                         val user = document.toObject(User::class.java)
                         Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
-                        user?.listPeminjmaanId?.remove(peminjamanId)
+                        user?.listPeminjamanId?.remove(peminjamanId)
                         database.collection("userUkm").document(currentUser!!.uid)
                             .set(user!!).addOnSuccessListener {
                                 Log.d("TEZ", "Current data: $it")
-                                savedPeminjamanId.postValue(user.listPeminjmaanId)
+                                savedPeminjamanId.postValue(user.listPeminjamanId)
                                 statusLiveData.postValue("SUCCESS")
                             }.addOnFailureListener {
                                 Log.d("TEZ", "Current data: $it")
@@ -243,14 +245,238 @@ class UserRepository {
     }
 
 
+    //    === Inventory ===
+    fun getSavedInventoryId(): MutableLiveData<ArrayList<String>> {
+        val currentUser = auth.currentUser
+        currentUser?.let {
+            database.collection("userUkm").document(it.uid)
+                .addSnapshotListener { snapshot, e ->
+                    if (e != null) {
+                        Log.w("TEZ", "Listen failed.", e)
+                        return@addSnapshotListener
+                    }
+
+                    if (snapshot != null && snapshot.exists()) {
+                        val allInventoryId = arrayListOf<String>()
+                        savedInventoryId.postValue(allInventoryId)
+                        val user = snapshot.toObject(User::class.java)
+                        Log.d(ContentValues.TAG, "DocumentSnapshot data: ${snapshot.data}")
+                        for (i in user!!.listInventoryId) {
+                            allInventoryId.add(i)
+                        }
+                        Log.d("TEZZ", "Current data: $allInventoryId")
+                        savedInventoryId.postValue(allInventoryId)
+                    } else {
+                        Log.d("TEZ", "Current data: null")
+                    }
+                }
+        }
+
+        return savedInventoryId
+    }
+
+    fun getSavedInventory(): MutableLiveData<ArrayList<Inventory>> {
+        val currentUser = auth.currentUser
+        currentUser?.let {
+            database.collection("userUkm").document(it.uid)
+                .addSnapshotListener { snapshot, e ->
+                    Log.d("TEZaZ", "Current data: $e")
+                    if (e != null) {
+                        Log.w("TEZ", "Listen failed.", e)
+                        return@addSnapshotListener
+                    }
+
+                    if (snapshot != null && snapshot.exists()) {
+                        val user = snapshot.toObject(User::class.java)
+                        val allInventory = arrayListOf<Inventory>()
+                        val allInventoryId = arrayListOf<String>()
+                        savedInventory.postValue(allInventory)
+                        Log.d(ContentValues.TAG, "DocumentSnapshot data: ${snapshot.data}")
+                        for (i in user!!.listInventoryId) {
+                            allInventoryId.add(i)
+                        }
+                        Log.d("TEZaZ", "Current data: $allInventoryId")
+                        database.collection("inventory")
+                            .get().addOnSuccessListener { mSnapshot ->
+                                if (mSnapshot != null && !mSnapshot.isEmpty) {
+                                    for (doc in mSnapshot) {
+                                        val mInventory = doc.toObject(Inventory::class.java)
+                                        if (allInventoryId.contains(mInventory.id)) {
+                                            allInventory.add(mInventory)
+                                            Log.d("TEZ", "Current data: $doc")
+                                        }
+                                    }
+                                    savedInventory.postValue(allInventory)
+                                } else {
+                                    Log.d("TEZ", "Current data: null")
+                                }
+                            }
+                    } else {
+                        Log.d("TEZ", "Current data: null")
+                    }
+                }
+        }
+
+        return savedInventory
+    }
+
+    suspend fun saveInventory(inventoryId: String): MutableLiveData<String> {
+        val currentUser = auth.currentUser
+        val statusLiveData = MutableLiveData<String>()
+        statusLiveData.postValue("LOADING")
+        currentUser?.let {
+            database.collection("userUkm").document(it.uid).get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val user = document.toObject(User::class.java)
+                        Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
+                        user?.listInventoryId?.add(inventoryId)
+                        database.collection("userUkm").document(currentUser!!.uid)
+                            .set(user!!).addOnSuccessListener {
+                                Log.d("TEZ", "Current data: $it")
+                                savedInventoryId.postValue(user.listInventoryId)
+                                statusLiveData.postValue("SUCCESS")
+                            }.addOnFailureListener {
+                                Log.d("TEZ", "Current data: $it")
+                                statusLiveData.postValue(it.toString())
+                            }
+                    } else {
+                        Log.d(ContentValues.TAG, "No such document")
+                        statusLiveData.postValue("No such document")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(ContentValues.TAG, "get failed with ", exception)
+                    statusLiveData.postValue(exception.toString())
+                }
+        }
+        return statusLiveData
+    }
+
+    suspend fun removeInventoryFromSaved(inventoryId: String): MutableLiveData<String> {
+        val currentUser = auth.currentUser
+        val statusLiveData = MutableLiveData<String>()
+        statusLiveData.postValue("LOADING")
+        currentUser?.let {
+            database.collection("userUkm").document(it.uid).get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val user = document.toObject(User::class.java)
+                        Log.d(ContentValues.TAG, "DocumentSnapshot data: ${document.data}")
+                        user?.listInventoryId?.remove(inventoryId)
+                        database.collection("userUkm").document(currentUser!!.uid)
+                            .set(user!!).addOnSuccessListener {
+                                Log.d("TEZ", "Current data: $it")
+                                savedInventoryId.postValue(user.listInventoryId)
+                                statusLiveData.postValue("SUCCESS")
+                            }.addOnFailureListener {
+                                Log.d("TEZ", "Current data: $it")
+                                statusLiveData.postValue(it.toString())
+                            }
+                    } else {
+                        Log.d(ContentValues.TAG, "No such document")
+                        statusLiveData.postValue("No such document")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(ContentValues.TAG, "get failed with ", exception)
+                    statusLiveData.postValue(exception.toString())
+                }
+        }
+        return statusLiveData
+    }
 
 
+    //    === Account ===
+    suspend fun changeEmailAndName(email: String, namaUkm: String): MutableLiveData<String> {
+        val user = auth.currentUser
+        val statusLiveData = MutableLiveData<String>()
+        statusLiveData.postValue("LOADING")
+        val profileUpdates = userProfileChangeRequest {
+            displayName = namaUkm
+        }
+        user!!.apply {
+            updateEmail(email)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(ContentValues.TAG, "User email address updated.")
+                        updateProfile(profileUpdates)
+                            .addOnCompleteListener { mTask ->
+                                if (mTask.isSuccessful) {
+                                    Log.d(ContentValues.TAG, "User email address updated.")
+                                    val mCurrentUser = auth.currentUser
+                                    val user = User(
+                                        mCurrentUser!!.uid,
+                                        mCurrentUser.displayName!!
+                                    )
+                                    statusLiveData.postValue("SUCCESS")
+                                    database.collection("userUkm")
+                                        .document(user.id)
+                                        .update("namaUkm", user.namaUkm)
+                                        .addOnSuccessListener {
+                                            _currentUserLiveData.postValue(auth.currentUser)
+                                        }.addOnFailureListener {
+                                            Log.d("TEZZ", "Error : $it")
+                                            statusLiveData.postValue("$it")
+                                            _currentUserLiveData.postValue(auth.currentUser)
+                                        }
+                                }
+                            }.addOnFailureListener {
+                                Log.d("TEZZ", "Error : $it")
+                                statusLiveData.postValue("$it")
+                                _currentUserLiveData.postValue(auth.currentUser)
+                            }
+                    }
+                }.addOnFailureListener {
+                    Log.d("TEZZ", "Error : $it")
+                    statusLiveData.postValue("$it")
+                    _currentUserLiveData.postValue(auth.currentUser)
+                }
+        }
+
+        return statusLiveData
+    }
+
+    suspend fun changePassword(oldPassword: String, newPassword: String): MutableLiveData<String> {
+        val user = auth.currentUser
+        val statusLiveData = MutableLiveData<String>()
+        statusLiveData.postValue("LOADING")
+        val credential = EmailAuthProvider.getCredential(
+            user?.email.toString(),
+            oldPassword
+        )
+
+        user?.reauthenticate(credential)
+            ?.addOnSuccessListener {
+                user.updatePassword(newPassword).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        _currentUserLiveData.value = auth.currentUser
+
+                        Log.w(ContentValues.TAG, task.toString())
+                        statusLiveData.postValue("SUCCESS")
+                    } else {
+                        Log.w(ContentValues.TAG, task.exception)
+                        statusLiveData.postValue("$it")
+                    }
+                }
+            }?.addOnFailureListener {
+                Log.w(ContentValues.TAG, it)
+                statusLiveData.postValue("$it")
+            }
+        return statusLiveData
+    }
 
 
-
-
-
-
-
-
+    companion object {
+        @Volatile
+        private var instance: UserRepository? = null
+        fun getInstance(): UserRepository {
+            return instance ?: synchronized(this) {
+                if (instance == null) {
+                    instance = UserRepository()
+                }
+                return instance as UserRepository
+            }
+        }
+    }
 }
